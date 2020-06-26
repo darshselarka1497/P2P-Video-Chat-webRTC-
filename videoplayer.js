62bfd307866effcb91a57c45b9f3a900b1004943
dselarka <dselarka@iu.edu>
2020-06-26 17:47:22 -0400

    added videoplayer.js to render the video call

--- videoplayer.js
+++ videoplayer.js
@@ -19,3 +19,22 @@ function Player(data){
     })
     document.body.appendChild(this.element)
 }
+
+Player.prototype.addStream = function(stream){
+    this.element.srcObject=stream
+    this.element.play()
+}
+
+Player.prototype.update = function(data){
+    data = data || {}
+    this.x = data.x || this.x
+    this.y = data.y || this.y
+    Object.assign(this.element.style,{
+        top: this.y + 'px',
+        left: this.x + 'px'
+    })
+}
+
+function randomColor(){
+    return '#'+Math.random().toString(16).substr(-6)
+}
